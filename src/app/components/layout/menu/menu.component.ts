574e4d4760d6d1a57d53dc420adb7c9436358317
JIgorSilva <jigorsilva@hotmail.com>
2025-03-01 11:52:35 -0300

    fix - ajuste componente menu para rederirizar o perfil, ajustes de rotas e layout.

--- src/app/components/layout/menu/menu.component.ts
+++ src/app/components/layout/menu/menu.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [MdbCarouselModule,RouterModule], // Remova o PerfilusuarioComponent daqui
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
-export class MenuComponent {
-
-}
+export class MenuComponent {}
