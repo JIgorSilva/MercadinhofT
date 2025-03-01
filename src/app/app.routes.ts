7634c4495fbb94605321a41b9de383ab805bdb7b
JIgorSilva <jigorsilva@hotmail.com>
2025-03-01 12:12:08 -0300

    Feat criação rotas editlayout front 02 (#5)
    * fix -  ajustes de dependencias, importes
    
    * fix -  ajustes na redenrixzação das rotas
    
    * fix - ajuste componente menu para rederirizar o perfil, ajustes de rotas e layout.
    
    * feat - implementação carrossel no  principalcomponent e ajustes de imports
    
    * feat - implementação dos alert swtich personalizdos
    
    * feat - criação component perfilusuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,21 +4,30 @@ import { PrincipalComponent } from './components/layout/principal/principal.comp
 import { CriarusuarioComponent } from './components/usuario/criarusuario/criarusuario.component';
 import { CriarprodutoComponent } from './components/produtos/criarproduto/criarproduto.component';
 import { ListarprodutoComponent } from './components/produtos/listarproduto/listarproduto.component';
+import { MenuComponent } from './components/layout/menu/menu.component';
+import { PerfilusuarioComponent } from './components/usuario/perfilusuario/perfilusuario.component';
 
-export const routes: Routes = [{ path: "", redirectTo: "login", pathMatch: "full" }, // Redireciona para login ao acessar "/"
-{ path: "login", component: LoginusuarioComponent },
-{
-    path: "usuario",
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redireciona para login ao acessar "/"
+  { path: 'login', component: LoginusuarioComponent },
+  {
+    path: 'menu',
+    component: MenuComponent,
+    children: [{ path: 'perfilusuario', component: PerfilusuarioComponent }]
+  },
+  {
+    path: 'usuario',
     component: PrincipalComponent,
     children: [
-        { path: "criar", component: CriarusuarioComponent }
+      { path: 'criar', component: CriarusuarioComponent }
     ]
-},
-{
-    path: "produtos",
+  },
+  {
+    path: 'produtos',
     component: PrincipalComponent,
     children: [
-        { path: "adicionar", component: CriarprodutoComponent },
-        { path: "listar", component: ListarprodutoComponent }
+      { path: 'adicionar', component: CriarprodutoComponent },
+      { path: 'listar', component: ListarprodutoComponent }
     ]
-},];
+  }
+];
